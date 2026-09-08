f2b4e61cad7907809ef17eb19decb040902e9020
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-07 21:32:48 -0300

    feat: agregar archivo vehiculo.py con clase Vehiculo

---
+++ vehiculo.py
@@ -0,0 +1,4 @@
+# Definición de la clase Vehiculo (se utiliza mayúscula inicial siguiendo las convenciones de estilo PEP 8 de Python)
+class Vehiculo:
+    # La instrucción 'pass' sirve como marcador de posición para indicar que la clase está vacía por el momento sin generar errores de sintaxis
+    pass
